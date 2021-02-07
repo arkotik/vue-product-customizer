cbbceb5651fa9ca9478d3f89381e042592306d7c
arkotik <arkotik@protonmail.com>
2021-02-07 13:57:45 +0200

    items arranging and removing, removing listenrs on change focus, some fixes

--- src/components/editor/Editor.vue
+++ src/components/editor/Editor.vue
@@ -2,11 +2,21 @@
   <div class="editor">
     <div class="items-list">
       <template v-for="(item, i) of shapes">
-        <div :key="i" class="item" :class="{ active: activeShape === i}" @click="setActive(item, i)">
+        <div :key="i" class="item" :class="{ active: activeShape === item.id}" @click="setActive(item)">
           <div class="thumb">
             <div v-if="item.shape === 'image'" :style="`background-image: url(${item.config.image.src}) !important`" :class="[item.shape]"/>
           </div>
-          <div class="title">{{ item.shape }}</div>
+          <div class="content">
+            <div class="title">{{ item.shape }}</div>
+            <div class="actions">
+              <button class="action action-button to-top" title="Bring to Front" @click.stop="arrangeItem(item, Infinity)"/>
+              <button class="action action-button up" title="Forward One" @click.stop="arrangeItem(item, 1)"/>
+              <button class="action action-button down" title="Back One" @click.stop="arrangeItem(item, -1)"/>
+              <button class="action action-button to-bottom" title="Send to Back" @click.stop="arrangeItem(item, -Infinity)"/>
+              <div class="divider"/>
+              <button class="action action-button remove" title="Remove" @click.stop="removeItem(item.id)"/>
+            </div>
+          </div>
         </div>
       </template>
     </div>
@@ -14,7 +24,7 @@
       <v-stage :config="editorConfig">
         <v-layer ref="stuffLayer">
           <template v-for="(item, i) of shapes">
-            <v-image v-if="item.shape === 'image'" :config="item.config" :key="i"/>
+            <v-image v-if="item.shape === 'image'" :config="item.config" :key="i" ref="shapes"/>
           </template>
         </v-layer>
         <v-layer ref="tplLayer">
@@ -22,7 +32,7 @@
         </v-layer>
         <v-layer ref="controlLayer">
           <template v-for="(item, i) of shapes">
-            <v-rect :config="getPhantomConfig(item)" :key="i" @mousedown="e => onShapeMouseDown(e, item, i)" :ref="`shape-${i}`"/>
+            <v-rect :config="getPhantomConfig(item)" :key="i" @mousedown="e => onShapeMouseDown(e, item)" ref="phantoms"/>
           </template>
           <v-transformer ref="transformer" :config="transformerConfig"/>
         </v-layer>
@@ -40,9 +50,58 @@ const snaps = Array(4)
   .fill([0, 30, 45, 60, 90])
   .reduce((acc, cur, i) => [...acc, ...cur.map(el => el + (i * 90))], []);
 
+function getCounter() {
+  let i = 0;
+  return () => i++;
+}
+
+const SHAPE_IMAGE = 'image';
+// const SHAPE_RECT = 'rect';
+
+const listenersMap = [
+  ['xChange', 'x'],
+  ['yChange', 'y'],
+  ['scaleXChange', 'scaleX'],
+  ['scaleYChange', 'scaleY'],
+  ['rotationChange', 'rotation'],
+];
 export default {
   name: 'Editor',
   methods: {
+    getIndex(id) {
+      return this.shapes.findIndex(el => el.id === id);
+    },
+    getItemById(id) {
+      return this.shapes.find(el => el.id === id);
+    },
+    getNodeById(id) {
+      const index = this.getIndex(id);
+      return {
+        phantom: this.$refs.phantoms[index]?.getNode(),
+        shape: this.$refs.shapes[index]?.getNode(),
+      };
+    },
+    arrangeItem(item, to) {
+      const { phantom, shape } = this.getNodeById(item.id);
+      if (!phantom || !shape) {
+        return;
+      }
+      const max = this.stuffLayer.children.length - 1;
+      let zIndex = shape.zIndex();
+      if (to === -Infinity) {
+        zIndex = 0;
+      } else if (to === Infinity) {
+        zIndex = max;
+      } else {
+        zIndex += to;
+      }
+      if (zIndex > max) {
+        return;
+      }
+      shape.zIndex(zIndex);
+      phantom.zIndex(zIndex);
+      this.stuffLayer.batchDraw();
+    },
     getPhantomConfig(item) {
       const { shape, config: { image, x, y, scaleX, scaleY, width, height, ...rest } } = item;
       const data = {
@@ -69,39 +128,50 @@ export default {
     addImage() {
       this.$refs.fileInput.click();
     },
-    setTransformer(node, item) { // TODO: remove event listeners
-      node.addEventListener('xChange', () => {
-        item.config.x = node.attrs.x;
-      });
-      node.addEventListener('yChange', () => {
-        item.config.y = node.attrs.y;
-      });
-      node.addEventListener('scaleXChange', () => {
-        item.config.scaleX = node.attrs.scaleX;
-      });
-      node.addEventListener('scaleYChange', () => {
-        item.config.scaleY = node.attrs.scaleY;
-      });
-      node.addEventListener('rotationChange', () => {
-        item.config.rotation = node.attrs.rotation;
-      });
+    setTransformer(node, item) {
+      const prev = this.activeShape;
+      if (item.id === prev) {
+        return;
+      }
+      this.activeShape = item.id;
+      if (prev !== null) {
+        const { phantom } = this.getNodeById(prev);
+        listenersMap.forEach(([event]) => phantom.removeEventListener(event));
+      }
+      listenersMap.forEach(([event, attribute]) => node.addEventListener(event, () => {
+        item.config[attribute] = node.attrs[attribute];
+      }));
       this.transformer.setNodes([node]);
-      this.stuffLayer.batchDraw();
-      this.transformer.zIndex(this.stuffLayer.children.length - 1);
+      this.controlLayer.batchDraw();
+      this.transformer.zIndex(this.controlLayer.children.length - 1);
     },
-    onShapeMouseDown(e, item, i) {
-      this.activeShape = i;
+    onShapeMouseDown(e, item) {
       this.setTransformer(e.target, item);
     },
-    setActive(item, i) {
-      const [el] = this.$refs[`shape-${i}`] || [];
+    setActive(item) {
+      const shapes = this.$refs.phantoms || [];
+      const index = this.shapes.findIndex(el => el.id === item.id);
+      const el = shapes[index];
       if (!el) {
         console.error('Can not get element');
         return;
       }
-      this.activeShape = i;
       this.setTransformer(el.getNode(), item);
     },
+    removeTransformer() {
+      this.transformer.setNodes([]);
+      this.stuffLayer.batchDraw();
+    },
+    removeItem(id) {
+      if (this.activeShape === id) {
+        this.removeTransformer();
+      }
+      this.activeShape = null;
+      this.shapes = this.shapes.filter(el => el.id !== id);
+    },
+    addShape(shape, config) {
+      this.shapes = [...this.shapes, { shape, config, id: this.next() }];
+    }
   },
   computed: {
     transformer() {
@@ -109,6 +179,9 @@ export default {
     },
     stuffLayer() {
       return this.$refs.stuffLayer.getNode();
+    },
+    controlLayer() {
+      return this.$refs.controlLayer.getNode();
     },
   },
   data() {
@@ -128,6 +201,7 @@ export default {
         rotationSnaps: snaps,
       },
       activeShape: null,
+      next: getCounter(),
     };
   },
   created() {
@@ -159,6 +233,9 @@ export default {
     // };
     this.$refs.fileInput.addEventListener('change', (e) => {
       const [file] = e.target.files;
+      if (!file) {
+        return;
+      }
       const url = URL.createObjectURL(file);
       const img = new window.Image();
       img.src = url;
@@ -175,7 +252,7 @@ export default {
           scaleY: scale,
           draggable: false,
         };
-        this.shapes = [...this.shapes, { shape: 'image', config: imgConf }];
+        this.addShape(SHAPE_IMAGE, imgConf);
       };
     });
   },
