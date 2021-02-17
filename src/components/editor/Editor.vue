87c773297efebc36dc7846bb5f13800c00cdf8fa
arkotik <arkotik@protonmail.com>
2021-02-17 17:19:32 +0200

    restyled, added simple text shape

--- src/components/editor/Editor.vue
+++ src/components/editor/Editor.vue
@@ -1,30 +1,45 @@
 <template>
   <div class="editor">
-    <div class="items-list">
-      <template v-for="(item, i) of shapes">
-        <div :key="i" class="item" :class="{ active: activeShape === item.id}" @click="setActive(item)">
-          <div class="thumb">
-            <div v-if="item.shape === 'image'" :style="`background-image: url(${item.config.image.src}) !important`" :class="[item.shape]"/>
-          </div>
-          <div class="content">
-            <div class="title">{{ item.shape }}</div>
-            <div class="actions">
-              <button class="action action-button to-top" title="Bring to Front" @click.stop="arrangeItem(item, Infinity)"/>
-              <button class="action action-button up" title="Forward One" @click.stop="arrangeItem(item, 1)"/>
-              <button class="action action-button down" title="Back One" @click.stop="arrangeItem(item, -1)"/>
-              <button class="action action-button to-bottom" title="Send to Back" @click.stop="arrangeItem(item, -Infinity)"/>
-              <div class="divider"/>
-              <button class="action action-button remove" title="Remove" @click.stop="removeItem(item.id)"/>
+    <div class="panel left-panel">
+      <div class="control-buttons">
+        <div class="buttons-group">
+          <div class="button primary" @click="addImage">+ Image</div>
+          <div class="button primary" @click="addText">+ Text</div>
+        </div>
+        <div class="buttons-group">
+          <div class="button danger" @click="clearAll">Clear all</div>
+        </div>
+      </div>
+      <div class="items-list">
+        <template v-for="(item, i) of shapes">
+          <div :key="i" class="item" :class="{ active: activeShape === item.id}" @click="setActive(item)">
+            <div class="thumb">
+              <div v-if="item.shape === 'image'" :style="`background-image: url(${item.config.image.src}) !important`" :class="[item.shape]"/>
+              <div v-if="item.shape === 'text'" :class="[item.shape]">
+                <svg viewBox="0 0 1024 1024"><path d="M213.333 170.667v128h234.667v512h128v-512h234.667v-128z"></path></svg>
+              </div>
+            </div>
+            <div class="content">
+              <div class="title">{{ item.shape }}</div>
+              <div class="actions">
+                <button class="action action-button to-top" title="Bring to Front" @click.stop="arrangeItem(item, Infinity)" :disabled="shapes.length < 2"/>
+                <button class="action action-button up" title="Forward One" @click.stop="arrangeItem(item, 1)" :disabled="shapes.length < 2"/>
+                <button class="action action-button down" title="Back One" @click.stop="arrangeItem(item, -1)" :disabled="shapes.length < 2"/>
+                <button class="action action-button to-bottom" title="Send to Back" @click.stop="arrangeItem(item, -Infinity)" :disabled="shapes.length < 2"/>
+                <div class="divider"/>
+                <button class="action action-button remove" title="Remove" @click.stop="removeItem(item.id)"/>
+              </div>
             </div>
           </div>
-        </div>
-      </template>
+        </template>
+      </div>
     </div>
     <div class="canvas-handler">
       <v-stage :config="editorConfig">
         <v-layer ref="stuffLayer">
           <template v-for="(item, i) of shapes">
             <v-image v-if="item.shape === 'image'" :config="item.config" :key="i" ref="shapes"/>
+            <v-text v-if="item.shape === 'text'" :config="item.config" :key="i" ref="shapes"/>
           </template>
         </v-layer>
         <v-layer ref="tplLayer">
@@ -38,10 +53,10 @@
         </v-layer>
       </v-stage>
     </div>
-    <div class="control-panel">
-      <button @click="addImage">Add image</button>
-      <input type="file" accept="image/*" hidden ref="fileInput">
+    <div class="panel right-panel">
+
     </div>
+    <input type="file" accept="image/*" hidden ref="fileInput">
   </div>
 </template>
 
@@ -56,7 +71,36 @@ function getCounter() {
 }
 
 const SHAPE_IMAGE = 'image';
+const SHAPE_TEXT= 'text';
 // const SHAPE_RECT = 'rect';
+
+function getTextConfig(config = {}) {
+  return {
+    x: 0,
+    y: 0,
+    text: 'VaporFly',
+    fontSize: 14,
+    align: 'center',
+    // fontFamily: 'Arial',
+    // fontStyle: 'normal',
+    // fontVariant: 'normal',
+    // textDecoration: '',
+    // verticalAlign: 'top',
+    // padding: 0,
+    // lineHeight: 1,
+    // wrap: 'word',
+    // ellipsis: false,
+    // fill: '#000000',
+    // fillEnabled: false,
+    // stroke: '#000000',
+    // strokeWidth: 1,
+    // strokeEnabled: false,
+    // visible: true,
+    // opacity: 1.0,
+    // rotation: 0,
+    ...config,
+  }
+}
 
 const listenersMap = [
   ['xChange', 'x'],
@@ -68,6 +112,11 @@ const listenersMap = [
 export default {
   name: 'Editor',
   methods: {
+    clearAll() {
+      this.removeTransformer();
+      this.activeShape = null;
+      this.shapes = [];
+    },
     getIndex(id) {
       return this.shapes.findIndex(el => el.id === id);
     },
@@ -128,6 +177,17 @@ export default {
     addImage() {
       this.$refs.fileInput.click();
     },
+    addText() {
+      const { width, height } = this.editorConfig;
+      const size = 32;
+      const config = getTextConfig({
+        y: ~~((height / 2) - (size / 2)),
+        width,
+        height: size,
+        fontSize: size
+      });
+      this.addShape(SHAPE_TEXT, config);
+    },
     setTransformer(node, item) {
       const prev = this.activeShape;
       if (item.id === prev) {
@@ -169,8 +229,8 @@ export default {
       this.activeShape = null;
       this.shapes = this.shapes.filter(el => el.id !== id);
     },
-    addShape(shape, config) {
-      this.shapes = [...this.shapes, { shape, config, id: this.next() }];
+    addShape(shape, config, extra = {}) {
+      this.shapes = [...this.shapes, { ...(extra || {}), shape, config, id: this.next() }];
     }
   },
   computed: {
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueKonva from 'vue-konva'
+import Vue from 'vue';
+import App from './App.vue';
+import VueKonva from 'vue-konva';
 import './assets/styles.scss';
 
-Vue.use(VueKonva)
+Vue.use(VueKonva);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
